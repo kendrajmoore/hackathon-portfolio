c855d204b531c3a793807de4055d09d1ae6e9b67
kendrajmoore <kendra.j.moore3443@gmail.com>
2021-06-09 10:04:35 -0400

    add template

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,28 @@ load_dotenv()
 app = Flask(__name__)
 
 
+
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/project')
+def project():
+    projects = [
+        {
+            'name': 'projectone',
+            'description': 'Beautiful day in Portland!',
+            'tools': 'React',
+            'link': 'www.google.com',
+            'source': 'www.github.com'
+        },
+        {
+            'name': 'projecttwo',
+            'description': 'Beautiful day in Georgia!',
+            'tools': 'Python',
+            'link': 'www.github.com',
+            'source': 'www.google.com'
+        }
+    ]
+
+    return render_template('project.html', projects=projects, url=os.getenv("URL"))
