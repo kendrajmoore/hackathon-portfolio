831a634b87c378b3f0159522d3a9746945803da5
armandopato <armauv@gmail.com>
2021-06-09 08:48:34 -0500

    Add profile route

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+
+@app.route('/profile')
+def get_profile():
+    return render_template('profile.html', title='Profile')
