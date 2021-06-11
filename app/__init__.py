a7a2deeca2cfe75d20bba99130a95d329b491d62
armandopato <armauv@gmail.com>
2021-06-11 00:36:20 -0500

    Fix title displaying

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ from helper import proj_data, proj_json, prof_json, prof_data
 load_dotenv()
 app = Flask(__name__)
 
+url = os.getenv("URL")
+
 
 @app.route('/')
 def index():
@@ -17,23 +19,23 @@ def index():
     projects = proj_json()
     profiles = prof_json()
     return render_template('index.html', profiles=profiles, projects=projects, title="Team Kenargi's portfolio",
-                           url=os.getenv("URL"),
+                           url=url,
                            img_1=img_1, img_2=img_2, img_3=img_3)
 
 
 @app.route('/projects/<name>')
 def get_project(name):
     item = proj_data(name)
-    return render_template('project.html', item=item, url=os.getenv("URL"))
+    return render_template('project.html', item=item, title=name, url=url + "/projects/" + name)
 
 
 @app.route('/profiles/<name>')
 def get_profile(name):
     item = prof_data(name)
-    return render_template('profile.html', item=item, title='Profile', url=os.getenv("URL"))
+    return render_template('profile.html', item=item, title=name + "'s Profile", url=url + "/profiles/" + name)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    source_img="/static/img/404.png"
+    source_img = "/static/img/404.png"
     return render_template('404.html', img=source_img, title="Page not found"), 404
